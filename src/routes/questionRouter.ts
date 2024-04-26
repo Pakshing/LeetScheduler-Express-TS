909aacff3013c2844b061750685af827f4dbc1c9
Pak Shing Kan <kennethkankps@gmail.com>
2024-04-26 15:48:24 -0400

    added CRUD for question entity

--- src/routes/questionRouter.ts
+++ src/routes/questionRouter.ts
@@ -10,7 +10,6 @@ import axios from 'axios';
 export default (router = express.Router()): void => {
 
     router.get('/questions/find', async(req:Request, res:Response) => {
-        console.log("req.header",req.headers)
         const email = getEmailFromToken(req, process.env.JWT_SECRET);
         console.log("email",email)
         const userRepository = myDataSource.getRepository(User);
@@ -22,7 +21,7 @@ export default (router = express.Router()): void => {
         if (!user) {
             return res.status(404).json({ message: 'User Not Found' });
         }
-        const questions = await questionRepository.find({ where: { owner: {id:user.id} } });
+        const questions = await questionRepository.find({ where: { owner: {id:user.id} }, order: { id: 'DESC' } });
 
         res.json(questions);
     });
@@ -31,7 +30,6 @@ export default (router = express.Router()): void => {
         const email = getEmailFromToken(req, process.env.JWT_SECRET);
         const userRepository = myDataSource.getRepository(User);
         const questionRepository = myDataSource.getRepository(Question);
-        const { url, title, difficulty, tags, next_review_in_days } = req.body;
         const last_completion = new Date();
         
         if (!email){
@@ -46,14 +44,14 @@ export default (router = express.Router()): void => {
         
         const existingQuestion = await questionRepository.findOne({
             where: {
-                title: title,
+                title: req.body.title,
                 owner: { id: user.id }
             },
             relations: ['owner']
         });
 
         if (existingQuestion) {
-            return res.status(400).json({ message: 'Question already exists' });
+            return res.status(400).json({ message: 'Question with the same title already exists' });
         }
 
         const newQuestion = questionRepository.create({
@@ -63,10 +61,71 @@ export default (router = express.Router()): void => {
         });
 
 
-
         const savedQuestion = await questionRepository.save(newQuestion);
 
         res.status(201).json(savedQuestion);
+    });
+
+    router.put('/questions/:id', async(req: Request, res: Response) => {
+        const email = getEmailFromToken(req, process.env.JWT_SECRET);
+        const userRepository = myDataSource.getRepository(User);
+        const questionRepository = myDataSource.getRepository(Question);
+        
+        if (!email){
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const user = await userRepository.findOne({ where: { email: email } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User Not Found' });
+        }
+
+        const question = await questionRepository.findOne({
+            where: {
+                id: Number(req.params.id),
+                owner: { id: user.id }
+            },
+            relations: ['owner']
+        });
+
+        if (!question) {
+            return res.status(404).json({ message: 'Question Not Found' });
+        }
+        questionRepository.merge(question, req.body.question);
+        const savedQuestion = await questionRepository.save(question);
+        res.status(200).json(savedQuestion);
+    });
+
+    router.delete('/questions/:id', async (req, res) => {
+        const email = getEmailFromToken(req, process.env.JWT_SECRET);
+        const userRepository = myDataSource.getRepository(User);
+        const questionRepository = myDataSource.getRepository(Question);
+        
+        if (!email){
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const user = await userRepository.findOne({ where: { email: email } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User Not Found' });
+        }
+
+        const question = await questionRepository.findOne({
+            where: {
+                id: Number(req.params.id),
+                owner: { id: user.id }
+            },
+            relations: ['owner']
+        });
+
+        if (!question) {
+            return res.status(404).json({ message: 'Question Not Found' });
+        }
+
+        await questionRepository.remove(question);
+        return res.status(200).json({ message: 'Question Deleted Successfully' });
     });
     
 }
